b247a24bd130584c673fc9d2d72fed23c7cb6b69
Ozlem Sigbeku <ozlemsigbeku@protonmail.com>
2023-03-05 21:32:57 +0000

    draw white square but broken

--- white_square.py
+++ white_square.py
@@ -1,27 +1,10 @@
 import pygame
 
-from pygame.locals import *
-
-class Square(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Square, self).__init__()
-        self.surf = pygame.Surface((100,100))
-        self.surf.fill((255,255,255))
-        self.rect = self.surf.get_rect()
-
 pygame.init()
 
-screen = pygame.display.set_mode((800,800))
+surface = pygame.display.set_mode((800,800))
+colour = (255, 255,255)
 
-square1 = Square()
-
-run = True
-
-while run:
-    for event in pygame.event.get():
-        screen.blit(square1.surf, (350,350))
-        if event.type == KEYDOWN:
-            if event.key == K_BACKSPACE:
-                run = False
-
-pygame.display.flip()
+while 1:
+    pygame.draw.rect(surface, colour, pygame.Rect(350, 350, 100, 100))
+    pygame.display.flip()
