f0eb05cd3a65bfde5426fa9fe85eaf02c398e961
Ozlem Sigbeku <ozlemsigbeku@protonmail.com>
2023-03-05 21:40:37 +0000

    draw square, handle events (h key and quit)

--- white_square.py
+++ white_square.py
@@ -1,10 +1,20 @@
 import pygame
 
+from pygame.locals import *
+
 pygame.init()
 
 surface = pygame.display.set_mode((800,800))
 colour = (255, 255,255)
 
-while 1:
+running = True
+
+while running:
     pygame.draw.rect(surface, colour, pygame.Rect(350, 350, 100, 100))
     pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_h:
+                print("Hello world!")
+        elif event.type == QUIT:
+            running = False
