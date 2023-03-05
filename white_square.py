17f568e23c6b7d9d4396e8b3bb7c35227cb0890b
Ozlem Sigbeku <ozlemsigbeku@protonmail.com>
2023-03-05 23:56:31 +0000

    moving the square with arrows

--- white_square.py
+++ white_square.py
@@ -9,12 +9,56 @@ colour = (255, 255,255)
 
 running = True
 
+left = 350
+top = 350
+
+sustained_up = False
+sustained_down = False
+sustained_right = False
+sustained_left = False
+sustained_h = False
+
 while running:
-    pygame.draw.rect(surface, colour, pygame.Rect(350, 350, 100, 100))
+    surface.fill((0, 0, 0))
+    pygame.draw.rect(surface, colour, pygame.Rect(top, left, 100, 100))
     pygame.display.flip()
+    if sustained_h:
+        print('Hello World!')
+    if sustained_down:
+        if left < 700:
+            left +=0.5
+    if sustained_up:
+        if left > 0:
+            left -=0.5
+    if sustained_right:
+        if top < 700:
+            top +=0.5
+    if sustained_left:
+        if top > 0:
+            top -=0.5
     for event in pygame.event.get():
         if event.type == KEYDOWN:
-            if event.key == K_h:
-                print("Hello world!")
+            if event.key == K_UP:
+                sustained_up = True
+            elif event.key == K_h:
+                sustained_h = True
+            elif event.key == K_DOWN:
+                sustained_down = True
+            elif event.key == K_RIGHT:
+                sustained_right = True
+            elif event.key == K_LEFT:
+                sustained_left = True
+        elif event.type == KEYUP:
+            if event.key == K_UP:
+                sustained_up = False
+            elif event.key == K_h:
+                sustained_h = False
+            elif event.key == K_DOWN:
+                sustained_down = False
+            elif event.key == K_RIGHT:
+                sustained_right = False
+            elif event.key == K_LEFT:
+                sustained_left = False
         elif event.type == QUIT:
             running = False
+    
