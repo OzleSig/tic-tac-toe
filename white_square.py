c80888d4d76d7e0b8387e319890f2e8adfaed056
Ozlem Sigbeku <ozlemsigbeku@protonmail.com>
2023-03-05 21:08:11 +0000

    copied code from geeksforgeeks

--- white_square.py
+++ white_square.py
@@ -1,3 +1,27 @@
 import pygame
 
-pygame.surface.fill((0, 0, 0))
+from pygame.locals import *
+
+class Square(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Square, self).__init__()
+        self.surf = pygame.Surface((100,100))
+        self.surf.fill((255,255,255))
+        self.rect = self.surf.get_rect()
+
+pygame.init()
+
+screen = pygame.display.set_mode((800,800))
+
+square1 = Square()
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        screen.blit(square1.surf, (350,350))
+        if event.type == KEYDOWN:
+            if event.key == K_BACKSPACE:
+                run = False
+
+pygame.display.flip()
