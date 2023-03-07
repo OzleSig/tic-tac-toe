b8702f0257426edc22dd74aaaeac4fc3ef30b7bf
Ozlem Sigbeku <ozlemsigbeku@protonmail.com>
2023-03-07 17:47:42 +0000

    added mouse location

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This is my repo for a pygame enhanced tic tac toe game.
 
 ## Progress so far
-I have played around with pygame and been able to draw a square which moves around with events (up, down, left and right). I have also drawn a grid with variables for sizing and positioning.
+I have played around with pygame and been able to draw a square which moves around with events (up, down, left and right). I have also drawn a grid with variables for sizing and positioning. I have added the mouse location (x, y) which is being printed on terminal when running.
 
 ## Planned changes
-I will be tracking mouse location and printing on terminal.
+Next I will be printing the coordinates of the grid the mouse is in instead of the screen location. Apparently this is called collision therapy.
--- white_square.py
+++ white_square.py
@@ -9,19 +9,11 @@ colour = (255, 255,255)
 
 running = True
 
-left = 350
-top = 350
-
-sustained_up = False
-sustained_down = False
-sustained_right = False
-sustained_left = False
-sustained_h = False
-
 size_of_boxes = 200
 position = (100,100)
 x = position[0]
 y = position[1]
+
 
 while running:
     surface.fill((0, 0, 0))
@@ -29,45 +21,9 @@ while running:
     pygame.draw.line(surface, colour, (x+size_of_boxes, y), (x+size_of_boxes, y+size_of_boxes*3), width= 5)
     pygame.draw.line(surface, colour, (x, y+size_of_boxes*2), (x+size_of_boxes*3, y+size_of_boxes*2), width= 5)
     pygame.draw.line(surface, colour, (x+size_of_boxes*2, y), (x+size_of_boxes*2, y+size_of_boxes*3), width= 5)
-    pygame.draw.rect(surface, colour, pygame.Rect(top, left, 100, 100))
     pygame.display.flip()
-    if sustained_h:
-        print('Hello World!')
-    if sustained_down:
-        if left < 700:
-            left +=0.5
-    if sustained_up:
-        if left > 0:
-            left -=0.5
-    if sustained_right:
-        if top < 700:
-            top +=0.5
-    if sustained_left:
-        if top > 0:
-            top -=0.5
+    mouse_pos = pygame.mouse.get_pos()
+    print(mouse_pos)
     for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if event.key == K_UP:
-                sustained_up = True
-            elif event.key == K_h:
-                sustained_h = True
-            elif event.key == K_DOWN:
-                sustained_down = True
-            elif event.key == K_RIGHT:
-                sustained_right = True
-            elif event.key == K_LEFT:
-                sustained_left = True
-        elif event.type == KEYUP:
-            if event.key == K_UP:
-                sustained_up = False
-            elif event.key == K_h:
-                sustained_h = False
-            elif event.key == K_DOWN:
-                sustained_down = False
-            elif event.key == K_RIGHT:
-                sustained_right = False
-            elif event.key == K_LEFT:
-                sustained_left = False
-        elif event.type == QUIT:
+        if event.type == QUIT:
             running = False
-    
