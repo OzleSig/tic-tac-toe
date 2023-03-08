353f58b413701a0dace9e7274321ebb8e7726e51
Ozlem Sigbeku <ozlemsigbeku@protonmail.com>
2023-03-08 00:59:16 +0000

    added square drawn on hover

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ This is my repo for a pygame enhanced tic tac toe game. I have already made a ti
 ## Progress so far
 * Drawn grid with variables for sizing and positioning (I might change this later to depend on the screen size)
 * Grid position of the mouse (collision detection)
+* Change the colour of the grid box hovered over
 
 ## Planned changes
-* Change the colour of the grid box hovered over
+* Draw X and O on grid
 * Bring in tic tac toe game logic
+* Player indicator and score
--- white_square.py
+++ white_square.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 pygame.init()
 
 surface = pygame.display.set_mode((800,800))
-colour = (255, 255,255)
+colour = (169, 169, 169)
 running = True
 size_of_boxes = 200
 position_of_box = (100,100)
@@ -29,16 +29,43 @@ def check_mouse_on_grid(mouse_pos):
         elif mouse_x < x3:
             x = 2
     if mouse_y > y0:
-        if mouse_y < y0:
+        if mouse_y < y1:
             y = 0
         elif mouse_y < y2:
             y = 1
         elif mouse_y < y3:
             y = 2
+#mouse is never (-1, y) or (x, -1)
+    if x == -1:
+        y = -1
+    elif y == -1:
+        x = -1
     return x,y
 
-def draw_hover()
-
+def draw_hover(mouse_pos):
+    hover_size = size_of_boxes*0.8
+    hover_pos = (x0, y0)
+    pos_multiplier = size_of_boxes*0.1
+    if not mouse_pos == (-1, -1):
+        if mouse_pos == (0, 0):
+            hover_pos = (x0, y0)
+        elif mouse_pos == (0, 1):
+            hover_pos = (x0, y1)
+        elif mouse_pos == (0, 2):
+            hover_pos = (x0, y2)
+        elif mouse_pos == (1, 0):
+            hover_pos = (x1, y0)
+        elif mouse_pos == (1, 1):
+            hover_pos = (x1, y1)
+        elif mouse_pos == (1, 2):
+            hover_pos = (x1, y2)
+        elif mouse_pos == (2, 0):
+            hover_pos = (x2, y0)
+        elif mouse_pos == (2, 1):
+            hover_pos = (x2, y1)
+        elif mouse_pos == (2, 2):
+            hover_pos = (x2, y2)
+        pygame.draw.rect(surface, colour, pygame.Rect(hover_pos[0]+pos_multiplier, hover_pos[1]+pos_multiplier, hover_size, hover_size))
 
 def game_draw():
     global running
@@ -48,10 +75,9 @@ def game_draw():
         pygame.draw.line(surface, colour, (x1, y0), (x1, y3), width= 5)
         pygame.draw.line(surface, colour, (x0, y2), (x3, y2), width= 5)
         pygame.draw.line(surface, colour, (x2, y0), (x2, y3), width= 5)
-        pygame.display.flip()
         mouse_pos = pygame.mouse.get_pos()
-        z = check_mouse_on_grid(mouse_pos)
-        print(z)
+        draw_hover(check_mouse_on_grid(mouse_pos))
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
